183707449a573b15dc7753d9a3f62cd5f6c994b6
Mostafa <mostafa.mohamed4111@gmail.com>
2019-08-29 19:30:03 +0200

    Cities

--- app/Http/Controllers/MasterData/CitiesController.php
+++ app/Http/Controllers/MasterData/CitiesController.php
@@ -2,10 +2,57 @@
 
 namespace App\Http\Controllers\MasterData;
 
+use App\Models\City;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class CitiesController extends Controller
 {
-    //
+    public function index()
+    {
+        $cities   =   City::query()->paginate(10);
+        return view('master-data.cities.index' , ['cities' => $cities]);
+    }
+
+    public function create(){
+        return view('master-data.cities.create');
+    }
+
+    public function store(Request $request) {
+        $this->validate($request , [
+            'gov_id'    =>  'required',
+            'en_name'   =>  'required|unique:cities,en_name',
+            'ar_name'   =>  'required|unique:cities,ar_name'
+        ]);
+
+        $gov    =   City::query()->create([
+            'en_name'   =>  $request->get('en_name'),
+            'ar_name'   =>  $request->get('ar_name'),
+            'is_active' =>  $request->has('is_active') ? 1 : 0,
+        ]);
+
+        return redirect()->action('MasterData\CitiesController@index')->with('success' , trans('global.city_created'));
+    }
+
+    public function edit($id){
+        $city    =   City::query()->findOrFail($id);
+        return view('master-data.cities.edit' , ['city' => $city]);
+    }
+
+    public function update(Request $request , $id) {
+        $this->validate($request , [
+            'gov_id'    =>  'required',
+            'en_name'   =>  'required|unique:cities,id',
+            'ar_name'   =>  'required|unique:cities,id'
+        ]);
+        $gov    =   City::query()->findOrFail($id);
+
+        $gov->update([
+            'en_name'   =>  $request->get('en_name'),
+            'ar_name'   =>  $request->get('ar_name'),
+            'is_active' =>  $request->has('is_active') ? 1 : 0,
+        ]);
+
+        return redirect()->action('MasterData\CitiesController@index')->with('success' , trans('global.city_updated'));
+    }
 }
---
+++ resources/views/master-data/cities/edit.blade.php
@@ -0,0 +1,82 @@
+@extends('layout.main')
+@section('content')
+
+    <div class="row">
+        <div class="col-12">
+            <h1>@lang('global.governorates')</h1>
+
+            <nav class="breadcrumb-container d-none d-sm-block d-lg-inline-block" aria-label="breadcrumb">
+                <ol class="breadcrumb pt-0">
+                    <li class="breadcrumb-item">
+                        <a href="#">@lang('global.master_data')</a>
+                    </li>
+                    <li class="breadcrumb-item">
+                        <a href="#">@lang('global.governorates')</a>
+                    </li>
+                    <li class="breadcrumb-item " aria-current="page">@lang('global.edit')</li>
+                </ol>
+            </nav>
+            <div class="separator mb-5"></div>
+        </div>
+    </div>
+
+    <br />
+    <br />
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div>
+                        <h5 class="mb-4">@lang('global.edit_governorates')</h5>
+                    </div>
+
+                    <form action="{{ route('governorates.update' , ['id' => $governorate->id ]) }}" method="post">
+                        @csrf
+                        @method('PUT')
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label>@lang('global.en_name')</label>
+                                <input type="text" class="form-control" name="en_name" value="{{ old('en_name' , $governorate->en_name) }}" placeholder="@lang('global.en_name')" required>
+                                @if($errors->has('en_name'))
+                                    <div id="jQueryName-error" class="error" style="">{{ $errors->first('en_name') }}</div>
+                                @endif
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputPassword1">@lang('global.ar_name')</label>
+                                <input type="text" class="form-control" id="inputPassword1" name="ar_name" value="{{ old('ar_name' , $governorate->ar_name) }}"
+                                       placeholder="@lang('global.ar_name')" required>
+                                @if($errors->has('ar_name'))
+                                    <div id="jQueryName-error" class="error" style="">{{ $errors->first('ar_name') }}</div>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-1">
+                            <label class="col-12 col-form-label">@lang('global.is_active')</label>
+                            <div class="col-12">
+                                <div class="custom-switch custom-switch-primary-inverse mb-2" style="padding-left: 0">
+                                    <input class="custom-switch-input" id="switch3" type="checkbox" value="1"
+                                           name="is_active" {{ old('is_active' , $governorate->is_active) == '1' ? 'checked' : '' }}>
+                                    <label class="custom-switch-btn" for="switch3"></label>
+                                </div>
+                                @if($errors->has('is_active'))
+                                    <div id="jQueryName-error" class="error" style="">{{ $errors->first('is_active') }}</div>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group col-md-12">
+                            <div class="float-right">
+                                <a href="{{ route('governorates.index') }}">
+                                    <button type="button" class="btn btn-danger btn-sm mt-3">@lang('global.cancel')</button>
+                                </a>
+                                <button type="submit" class="btn btn-primary btn-sm mt-3">@lang('global.save')</button>
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
