7cf7645f2d3b30bd63d6f8c178d437837f7727bb
Radwa Hosny <radwahosny341994@gmail.com>
2019-09-05 16:39:36 +0200

    edit user functionality testing

---
+++ cypress/integration/edit-user.js
@@ -0,0 +1,101 @@
+describe('Edit Users test', function () {
+
+
+    beforeEach(function () {
+        cy.visit('http://127.0.0.1:8000');
+        cy.wait(2000);
+        cy.url().should('contain', '/login');
+        cy.get(':nth-child(2) > .form-control').type('admin');
+        cy.get(':nth-child(3) > .form-control').type('123456');
+        cy.contains('Sign In').click();
+        cy.wait(2000);
+        cy.url().should('eq', 'http://127.0.0.1:8000/');
+        cy.exec('php artisan test:delete_user');
+        cy.exec('php artisan test:create_user');
+        cy.exec('php artisan test:delete_user2');
+    })
+
+    afterEach(function () {
+        cy.exec('php artisan test:delete_user');
+    })
+
+    it('check nullable values', function () {
+        cy.contains('Master Data').click()
+        cy.get('.sidebar-sub.sidebar-sub-users').click();
+        cy.url().should('contain', '/master-data/users');
+        cy.get("a[href|='http://127.0.0.1:8000/master-data/users/1000/edit']").click();
+        cy.get('input[name="employee_code"]').clear();
+        cy.get('input[name="email"]').clear();
+        cy.get('select[name=role_id]').select('Select Role');
+        cy.contains('Save').click();
+        cy.contains('User Updated Success').should('be.visible');
+    })
+
+    it('check default inactive status', function () {
+        cy.contains('Master Data').click()
+        cy.get('.sidebar-sub.sidebar-sub-users').click();
+        cy.url().should('contain', '/master-data/users');
+        cy.get("a[href|='http://127.0.0.1:8000/master-data/users/1000/edit']").click();
+        cy.get('.custom-switch-btn').should('have.css', 'border', '1px solid rgb(215, 215, 215)');
+    })
+
+    it('matching password confirmation', function () {
+        cy.contains('Master Data').click()
+        cy.get('.sidebar-sub.sidebar-sub-users').click();
+        cy.url().should('contain', '/master-data/users');
+        cy.get("a[href|='http://127.0.0.1:8000/master-data/users/1000/edit']").click();
+        cy.get('input[name=password]').type('123456');
+        cy.get('input[name=password_confirmation]').type('123459');
+        cy.contains('Save').click();
+        cy.contains('The password confirmation does not match.').should('be.visible');
+    })
+    it('check password lenghth', function () {
+        cy.contains('Master Data').click()
+        cy.get('.sidebar-sub.sidebar-sub-users').click();
+        cy.url().should('contain', '/master-data/users');
+        cy.get("a[href|='http://127.0.0.1:8000/master-data/users/1000/edit']").click();
+        cy.get('input[name=password]').type('12345');
+        cy.get('input[name=password_confirmation]').type('12345');
+        cy.contains('Save').click();
+        cy.contains('The password must be at least 6 characters.').should('be.visible');
+    })
+    it('required fields check', function () {
+        cy.contains('Master Data').click()
+        cy.get('.sidebar-sub.sidebar-sub-users').click();
+        cy.url().should('contain', '/master-data/users');
+        cy.get("a[href|='http://127.0.0.1:8000/master-data/users/1000/edit']").click();
+        cy.get('input[name="full_name"]').should('have.attr', 'required');
+        cy.get('input[name="user_name"]').should('have.attr', 'required');
+    })
+    it('default language check', function () {
+        cy.contains('Master Data').click()
+        cy.get('.sidebar-sub.sidebar-sub-users').click();
+        cy.url().should('contain', '/master-data/users');
+        cy.get("a[href|='http://127.0.0.1:8000/master-data/users/1000/edit']").click();
+        cy.get('select[name="lang"]').should('have.value', 'ar');
+    })
+    it('check user name length', function () {
+        cy.contains('Master Data').click()
+        cy.get('.sidebar-sub.sidebar-sub-users').click();
+        cy.url().should('contain', '/master-data/users');
+        cy.get("a[href|='http://127.0.0.1:8000/master-data/users/1000/edit']").click();
+        cy.get('input[name=user_name]').type('t');
+        cy.contains('Save').click();
+        cy.contains('The user name must be at least 4 characters.').should('be.visible');
+    })
+    it('unique fields check', function () {
+        cy.exec('php artisan test:create_user2');
+        cy.contains('Master Data').click();
+        cy.get('.sidebar-sub.sidebar-sub-users').click();
+        cy.url().should('contain', '/master-data/users');
+        cy.get("a[href|='http://127.0.0.1:8000/master-data/users/1001/edit']").click();
+        cy.get('input[name=user_name]').type('test');
+        cy.contains('Save').click();
+        cy.contains('The user name has already been taken.').should('be.visible');
+        cy.get('input[name=email]').type('test@test.com');
+        cy.contains('Save').click();
+        cy.contains('The email has already been taken.').should('be.visible');
+        cy.exec('php artisan test:delete_user2');
+    })
+
+})
--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\User;
 use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,44 @@ use Illuminate\Foundation\Inspiring;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
+})->describe('Display an inspiring quote');
+
+Artisan::command('test:create_user', function () {
+    DB::table('users')->insert([
+        'id'=> 1000,
+        'full_name'         =>  'test',
+        'user_name'         =>  'test',
+        'employee_number'   =>  10000,
+        'email'             =>  'test@test.com',
+        'password'          =>  Hash::make(123456),
+        'is_active'         => 0
+    ]);
+})->describe('Display an inspiring quote');
+
+Artisan::command('test:delete_user', function () {
+    optional(User::find(1000))->delete();
+})->describe('Display an inspiring quote');
+
+
+
+
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->describe('Display an inspiring quote');
+
+Artisan::command('test:create_user2', function () {
+    DB::table('users')->insert([
+        'id'=> 1001,
+        'full_name'         =>  'test2',
+        'user_name'         =>  'test2',
+        'employee_number'   =>  10001,
+        'email'             =>  'test2@test.com',
+        'password'          =>  Hash::make(123456),
+        'is_active'         => 0
+    ]);
+})->describe('Display an inspiring quote');
+
+Artisan::command('test:delete_user2', function () {
+    optional(User::find(1001))->delete();
 })->describe('Display an inspiring quote');
