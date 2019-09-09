67de3d8ace65dfb9d7e52aea1dca67e1ef3afcee
Mahmoud Hosny <mahmoud.hosny@sanatechnology.com>
2019-09-09 16:31:04 +0200

    fix permissions name

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -32,7 +32,7 @@ class PermissionsTableSeeder extends Seeder
         Permission::create([
             'sub_menu_id' => $subMenu->id,
             'name' => 'roles.index',
-            'en_display_name' => 'Roles Index',
+            'en_display_name' => 'List Roles',
             'ar_display_name' => 'عرض الصلاحيات',
             'en_description' => NULL,
             'ar_description' => NULL,
@@ -40,7 +40,7 @@ class PermissionsTableSeeder extends Seeder
         Permission::create([
             'sub_menu_id' => $subMenu->id,
             'name' => 'roles.create',
-            'en_display_name' => 'Roles',
+            'en_display_name' => 'Add Roles',
             'ar_display_name' => 'إضافة الصلاحيات',
             'en_description' => NULL,
             'ar_description' => NULL,
@@ -49,7 +49,7 @@ class PermissionsTableSeeder extends Seeder
         Permission::create([
             'sub_menu_id' => $subMenu->id,
             'name' => 'roles.edit',
-            'en_display_name' => 'Roles',
+            'en_display_name' => 'Edit Roles',
             'ar_display_name' => 'تعديل الصلاحيات',
             'en_description' => NULL,
             'ar_description' => NULL,
@@ -60,7 +60,7 @@ class PermissionsTableSeeder extends Seeder
         Permission::create([
             'sub_menu_id' => $subMenu->id,
             'name' => 'users.index',
-            'en_display_name' => 'Users',
+            'en_display_name' => 'List Users',
             'ar_display_name' => 'عرض المستخدمين',
             'en_description' => NULL,
             'ar_description' => NULL,
@@ -69,7 +69,7 @@ class PermissionsTableSeeder extends Seeder
         Permission::create([
             'sub_menu_id' => $subMenu->id,
             'name' => 'users.create',
-            'en_display_name' => 'Users',
+            'en_display_name' => 'Add Users',
             'ar_display_name' => 'إضافة المستخدمين',
             'en_description' => NULL,
             'ar_description' => NULL,
@@ -77,7 +77,7 @@ class PermissionsTableSeeder extends Seeder
         Permission::create([
             'sub_menu_id' => $subMenu->id,
             'name' => 'users.edit',
-            'en_display_name' => 'Users',
+            'en_display_name' => 'Edit Users',
             'ar_display_name' => 'تعديل المستخدمين',
             'en_description' => NULL,
             'ar_description' => NULL,
@@ -87,7 +87,7 @@ class PermissionsTableSeeder extends Seeder
         Permission::create([
             'sub_menu_id' => $subMenu->id,
             'name' => 'governorates.index',
-            'en_display_name' => 'Governorate Index',
+            'en_display_name' => 'List Governorate',
             'ar_display_name' => 'عرض المحافظات',
             'en_description' => NULL,
             'ar_description' => NULL,
@@ -96,7 +96,7 @@ class PermissionsTableSeeder extends Seeder
             'sub_menu_id' =>  $subMenu->id,
 
             'name' => 'governorates.create',
-            'en_display_name' => 'Governorate Create',
+            'en_display_name' => 'Create Governorate',
             'ar_display_name' => 'إضافة المحافظات',
             'en_description' => NULL,
             'ar_description' => NULL,
@@ -104,7 +104,7 @@ class PermissionsTableSeeder extends Seeder
         Permission::create([
             'sub_menu_id' =>  $subMenu->id,
             'name' => 'governorates.edit',
-            'en_display_name' => 'Governorate Edit',
+            'en_display_name' => 'Edit Governorate',
             'ar_display_name' => 'تعديل المحافظات',
             'en_description' => NULL,
             'ar_description' => NULL,
@@ -115,7 +115,7 @@ class PermissionsTableSeeder extends Seeder
         Permission::create([
             'sub_menu_id' => $subMenu->id,
             'name' => 'cities.index',
-            'en_display_name' => 'Cities Index',
+            'en_display_name' => 'List Cities',
             'ar_display_name' => 'عرض المدن',
             'en_description' => NULL,
             'ar_description' => NULL,
@@ -123,7 +123,7 @@ class PermissionsTableSeeder extends Seeder
         Permission::create([
             'sub_menu_id' => $subMenu->id,
             'name' => 'cities.create',
-            'en_display_name' => 'Cities Create',
+            'en_display_name' => 'Create Cities',
             'ar_display_name' => 'إضافة المدن',
             'en_description' => NULL,
             'ar_description' => NULL,
@@ -131,7 +131,7 @@ class PermissionsTableSeeder extends Seeder
         Permission::create([
             'sub_menu_id' => $subMenu->id,
             'name' => 'cities.edit',
-            'en_display_name' => 'Cities Edit',
+            'en_display_name' => 'Edit Cities',
             'ar_display_name' => 'تعديل المدن',
             'en_description' => NULL,
             'ar_description' => NULL,
@@ -141,7 +141,7 @@ class PermissionsTableSeeder extends Seeder
         Permission::create([
             'sub_menu_id' => $subMenu->id,
             'name' => 'centers.index',
-            'en_display_name' => 'Centers Index',
+            'en_display_name' => 'List Centers',
             'ar_display_name' => 'عرض المراكز',
             'en_description' => NULL,
             'ar_description' => NULL,
@@ -149,7 +149,7 @@ class PermissionsTableSeeder extends Seeder
         Permission::create([
             'sub_menu_id' => $subMenu->id,
             'name' => 'centers.create',
-            'en_display_name' => 'Centers Create',
+            'en_display_name' => 'Create Centers',
             'ar_display_name' => 'إضافة المراكز',
             'en_description' => NULL,
             'ar_description' => NULL,
@@ -157,7 +157,7 @@ class PermissionsTableSeeder extends Seeder
         Permission::create([
             'sub_menu_id' => $subMenu->id,
             'name' => 'centers.edit',
-            'en_display_name' => 'Centers Edit',
+            'en_display_name' => 'Edit Centers',
             'ar_display_name' => 'تعديل المراكز',
             'en_description' => NULL,
             'ar_description' => NULL,
