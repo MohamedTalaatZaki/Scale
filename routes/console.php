919c1d39a222b7e80e2f1be27db1bc4ddb8a5fe8
Radwa Hosny <radwahosny341994@gmail.com>
2019-09-11 15:08:03 +0200

    fix conflict

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\User;
 use App\Models\Roles\Permission;
 use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +21,45 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
 
+Artisan::command('test:create_user', function () {
+    DB::table('users')->insert([
+        'id'=> 1000,
+        'full_name'         =>  'test',
+        'user_name'         =>  'testing',
+        'employee_number'   =>  10000,
+        'email'             =>  'test@testing.com',
+        'password'          =>  Hash::make(123456),
+        'is_active'         => 0
+    ]);
+})->describe('Display an inspiring quote');
+
+Artisan::command('test:delete_user', function () {
+    optional(User::find(1000))->delete();
+})->describe('Display an inspiring quote');
+
+
+
+
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->describe('Display an inspiring quote');
+
+Artisan::command('test:create_user2', function () {
+    DB::table('users')->insert([
+        'id'=> 1001,
+        'full_name'         =>  'test2',
+        'user_name'         =>  'test2',
+        'employee_number'   =>  10001,
+        'email'             =>  'test2@test.com',
+        'password'          =>  Hash::make(123456),
+        'is_active'         => 0
+    ]);
+})->describe('Display an inspiring quote');
+
+Artisan::command('test:delete_user2', function () {
+    optional(User::find(1001))->delete();
+})->describe('Display an inspiring quote');
 Artisan::command('user:add_permission {permission}', function ($permission) {
     $permission = Permission::where('name',$permission)->first();
     if(!is_null($permission)){
@@ -34,6 +76,7 @@ Artisan::command('user:remove_permission {permission}', function ($permission) {
         optional($role)->detachPermission($permission);
     }
 })->describe('remove permission to test role');
+
 
 
 
