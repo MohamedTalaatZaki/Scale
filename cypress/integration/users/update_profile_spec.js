75216834c9eb3e6c6feaaad71ad7f86d32bcdb68
tonaguy <tonaguy.zaki@gmail.com>
2019-09-12 12:15:23 +0200

    set_default_lang_to_test_user_command

--- cypress/integration/users/update_profile_spec.js
+++ cypress/integration/users/update_profile_spec.js
@@ -1,17 +1,15 @@
 beforeEach(function () {
-    cy.visit('/');
-    cy.wait(2000);
-    cy.url().should('contain', '/login');
-    cy.get(':nth-child(2) > .form-control').type('test')
-    cy.get(':nth-child(3) > .form-control').type('123456')
-    cy.contains('Sign In').click();
-    cy.get('.name').click();
-    cy.get('a[data-target="#account-info"]').click();
-    cy.get('.modal-content form').invoke('attr', 'noValidate','true');
-})
-before(function(){
   cy.exec('php artisan user:setLocale en');
-});
+  cy.visit('/');
+  cy.wait(2000);
+  cy.url().should('contain', '/login');
+  cy.get(':nth-child(2) > .form-control').type('test')
+  cy.get(':nth-child(3) > .form-control').type('123456')
+  cy.contains('Sign In').click();
+  cy.get('.name').click();
+  cy.get('a[data-target="#account-info"]').click();
+  cy.get('.modal-content form').invoke('attr', 'noValidate','true');
+})
 describe('update profile settings',function(){
   it('updates default language successfully', function () {
     cy.get('#inputState1> option[value="dark"]').invoke('attr', 'selected',true);
@@ -39,22 +37,22 @@ describe('update profile settings',function(){
     cy.get('#password').clear().type('123');
     cy.get('#password_confirmation').clear().type('123');
     cy.get('.modal-footer > .btn-primary').click();
-    cy.get('form-row > form-group col-md-6').should('contain','The password must be at least 6 characters.');
+    cy.get('.error').should('contain','The password must be at least 6 characters.');
   })
   it('checks password confirmation',function(){
     cy.get('#password').clear().type('123456');
     cy.get('#password_confirmation').clear().type('12345');
     cy.get('.modal-footer > .btn-primary').click();
-    cy.get('form-row > form-group col-md-6').should('contain','The password confirmation does not match.');
+    cy.get('.error').should('contain','The password confirmation does not match.');
   })
   it('checks cancel button',function(){
-    cy.get('#inputState1> option[value="dark"]').invoke('attr', 'selected',true);
+    cy.get('#inputState1> option[value="light"]').invoke('attr', 'selected',true);
     cy.get('#inputState2> option[value="ar"]').invoke('attr', 'selected',true);
     cy.get('.btn-secondary').click();
     cy.server();
     cy.request('get','/api/user/test').then((response)=>{
       expect(response.body).to.have.property('lang', 'en')
-      expect(response.body).to.have.property('theme', 'light')
+      expect(response.body).to.have.property('theme', 'dark')
     });
   })
   it('checks password update button',function(){
