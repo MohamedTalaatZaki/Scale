61a74c5e91ad68348665de1123191c479200f6c0
Mostafa <mostafa.mohamed4111@gmail.com>
2019-09-24 12:09:04 +0200

    fix-conflict

--- resources/lang/en/global.php
+++ resources/lang/en/global.php
@@ -146,5 +146,4 @@ return [
     'min_max_error' =>  'The Min Input Must Be Lower Than Max Input',
     'created_success'   =>  'Created Successful',
     'edit_success'   =>  'Updated Successful',
-
 ];
