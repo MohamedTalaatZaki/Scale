f6e425e9df7b18118f49cd9defec6848e3ed6f3f
Mostafa <mostafa.mohamed4111@gmail.com>
2019-10-08 15:54:22 +0200

    Edit

--- app/Http/Controllers/Security/TrucksArrivalController.php
+++ app/Http/Controllers/Security/TrucksArrivalController.php
@@ -107,6 +107,33 @@ class TrucksArrivalController extends Controller
         return $this->index()->with(['truckArrival'  =>  $truckArrival]);
     }
 
+    public function update(Request $request , $id)
+    {
+        $this->validate($request , [
+            'driver_name'  =>  'required',
+            'driver_license'  =>  'required',
+            'driver_national_id'  =>  'required|digits:14|numeric',
+            'driver_mobile'  =>  'required|digits:11|numeric',
+            'supplier_id'  =>  'required|exists:suppliers,id',
+            'governorate_id'  =>  'required|exists:governorates,id',
+            'city_id'  =>  'required||exists:cities,id|exists:cities,id',
+            'center_id' =>  'nullable||exists:centers,id|numeric',
+            'truck_type_id'  =>  'required|exists:trucks_types,id',
+            'truck_plates_tractor'  =>  'required',
+            'item_type_id'  =>  'required|exists:item_types,id',
+            'item_group_id'  =>  [new RequiredIfItemTypeRaw()],
+            'theoretical_weight'  =>  [new RequiredIfItemTypeRaw()],
+        ]);
+
+
+        $request->offsetSet('status' , isItemTypeRaw($request->input('item_type_id')) ? 'arrived' : 'waiting');
+        $truck  =   TruckArrival::query()->find($id);
+
+        $truck->update($request->input());
+
+        return redirect()->action('Security\TrucksArrivalController@index')->with('success' , trans('global.updated_success'));
+    }
+
     public function inProcess(Request $request)
     {
         $truck  =   TruckArrival::query()->find($request->get('id'));
