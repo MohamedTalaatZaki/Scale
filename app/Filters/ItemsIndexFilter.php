a2ae6c386a102440c8f999993776c180891367b9
Mostafa <mostafa.mohamed4111@gmail.com>
2019-10-16 13:23:05 +0200

    Supplier Items Button - modification

--- app/Filters/ItemsIndexFilter.php
+++ app/Filters/ItemsIndexFilter.php
@@ -7,12 +7,14 @@ namespace App\Filters;
 use App\Filters\Item\ArNameFilter;
 use App\Filters\Item\EnNameFilter;
 use App\Filters\Item\SapCodeFilter;
+use App\Filters\Item\SupplierFilter;
 
 class ItemsIndexFilter extends AbstractFilter
 {
     protected $filters  =   [
-        'ar_name'   =>  ArNameFilter::class,
-        'en_name'   =>  EnNameFilter::class,
-        'sap_code'  =>  SapCodeFilter::class
+        'ar_name'       =>  ArNameFilter::class,
+        'en_name'       =>  EnNameFilter::class,
+        'sap_code'      =>  SapCodeFilter::class,
+        'supplier_id'   =>  SupplierFilter::class
     ];
 }
