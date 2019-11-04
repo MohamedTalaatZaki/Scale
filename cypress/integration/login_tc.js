1c73f7b790746a53054043f50bda4076a7406846
tonaguy <tonaguy.zaki@gmail.com>
2019-11-04 10:55:41 +0200

    JUH-52 login-function-for-testing

--- cypress/integration/login_tc.js
+++ cypress/integration/login_tc.js
@@ -1,10 +1,14 @@
 describe('login', function () {
-   cy.open_app_bb();
-   cy.login_bb();
-   cy.assert_success_login_bb();
-   cy.login_out_bb();
+  it('test login',()=>{
+    cy.open_app_bb('/');
+    cy.login_bb();
+    cy.assert_success_login_bb();
+    cy.login_out_bb();
 
-   cy.login_bb('invalid user');
-   cy.assert_fail_login_bb();
+    cy.login_bb('invalid user');
+    cy.wait(2000);
+    cy.assert_fail_login_bb();
+  });
+
 
 });
