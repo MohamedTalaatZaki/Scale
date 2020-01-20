e235d9abe4551d4603fc0ab372ea8edae9ed2630
Mostafa <mostafa.mohamed4111@gmail.com>
2020-01-20 20:57:56 +0200

    Dashboard Icons

--- resources/views/dashboard/widgets/counters/rejected-tons.blade.php
+++ resources/views/dashboard/widgets/counters/rejected-tons.blade.php
@@ -2,7 +2,7 @@
     @if(request()->input('filter_item_type') == 'raw')
         <a href="#" class="card" style="cursor: default">
             <div class="card-body text-center">
-                <i class="iconsminds-clock"></i>
+                <i class="iconsminds-close"></i>
                 <p class="card-text mb-0">@lang('global.rejected')</p>
                 <div class="container">
                     <div class="row">
