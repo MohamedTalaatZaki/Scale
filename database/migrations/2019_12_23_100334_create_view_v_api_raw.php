cec322c8f24050792e1ca4236ad57cdf5b278d1b
mohamedtalaatzaki <mohamed.talaat@sanatechnology.com>
2020-01-21 17:02:04 +0200

    'server_v_api_update'

--- database/migrations/2019_12_23_100334_create_view_v_api_raw.php
+++ database/migrations/2019_12_23_100334_create_view_v_api_raw.php
@@ -17,40 +17,41 @@ class CreateViewVApiRaw extends Migration
         DB::statement("
                 Create View v_api_raw
                 As
-                Select
-                      th.supplier_id                                                  as VENDORKEY
-                     ,S.ar_name                                                       as VENDORNAME
-                     ,i.sap_code                                                      as MATERIALKEY
-                     ,i.ar_name                                                       as MATERIALNAME
-                     ,(tl.weight_in/1000)                                             as WEIGHT1
-                     ,format(tl.weight_in_date , 'yyyy-MM-d')                         as DATE1
-                     ,format(tl.weight_in_date , 'HH:mm:ss')                          as TIME1
-                     ,(tl.weight_out/1000)                                            as WEIGHT2
-                     ,format(tl.weight_out_date , 'yyyy-MM-d')                        as DATE2
-                     ,format(tl.weight_out_date , 'HH:mm:ss')                         as TIME2
-                     ,(tl.weight/1000)                                                as NETWEIGHT
-                     ,td.id                                                           as SERIAL
-                     ,cast(th.transport_number AS varchar)+'-'+CAST(td.id AS varchar) as TICKET
-                     ,td.truck_plates                                                 as TRUCNO
-                     ,tl.batch_number                                                 as CHARG
-                     ,td.discount                                                     as DISCOUNT
-                     ,th.arrival_time                                                 as post_date
-                from transports th left join transport_details td
-                on (th.id=td.transport_id)
-                left join transport_lines tl
-                on (td.id=tl.transport_detail_id)
-                left join item_types it
-                on (it.id=th.item_type_id)
-                left join item_group ig
-                on (ig.id=td.item_group_id)
-                left join items i
-                on (i.id=td.item_id)
-                left join suppliers s
-                on (s.id=th.supplier_id)
-                left join lines l
-                on (l.id=tl.line_id)
-                Where i.item_type_id = (select id from item_types where prefix = 'raw')
-                and   td.posted_at is null;
+                Select  
+                         s.sap_code                                                      as VENDORKEY  
+                        ,S.ar_name                                                       as VENDORNAME  
+                        ,i.sap_code                                                      as MATERIALKEY  
+                        ,i.ar_name                                                       as MATERIALNAME  
+                        ,(tl.weight_in/1000)                                             as WEIGHT1  
+                        ,format(tl.weight_in_date , 'yyyy-MM-d')                         as DATE1  
+                        ,format(tl.weight_in_date , 'HH:mm:ss')                          as TIME1  
+                        ,(tl.weight_out/1000)                                            as WEIGHT2  
+                        ,format(tl.weight_out_date , 'yyyy-MM-d')                        as DATE2  
+                        ,format(tl.weight_out_date , 'HH:mm:ss')                         as TIME2  
+                        ,(tl.weight/1000)                                                as NETWEIGHT  
+                        ,td.id                                                           as SERIAL  
+                        ,cast(th.transport_number AS varchar)+'-'+CAST(td.id AS varchar) as TICKET  
+                        ,td.truck_plates                                                 as TRUCNO  
+                        ,tl.batch_number                                                 as CHARG  
+                        ,td.discount                                                     as DISCOUNT  
+                        ,Cast(th.arrival_time as date)                                   as post_date  
+                from transports th left join transport_details td  
+                on (th.id=td.transport_id)  
+                left join transport_lines tl  
+                on (td.id=tl.transport_detail_id)  
+                left join item_types it  
+                on (it.id=th.item_type_id)  
+                left join item_group ig  
+                on (ig.id=td.item_group_id)  
+                left join items i  
+                on (i.id=td.item_id)  
+                left join suppliers s  
+                on (s.id=th.supplier_id)  
+                left join lines l  
+                on (l.id=tl.line_id)  
+                Where i.item_type_id = (select id from item_types where prefix = 'raw')  
+                and  (tl.weight_in is not null and tl.weight_out is not null) 
+                and   td.posted_at is null;  
         ");
     }
 
